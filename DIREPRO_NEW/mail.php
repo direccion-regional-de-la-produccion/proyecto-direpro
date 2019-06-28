214dce49cdaec59351bdaf08809abcf9a59c1184
Fernando Santa Cruz Arroyo <nando.1830@gmail.com>
2019-06-28 14:21:31 -0500

    Actualizando DIREPRO

---
+++ DIREPRO_NEW/mail.php
@@ -0,0 +1,69 @@
+
+
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link href="css/pan_p.css" type="text/css" rel="stylesheet"/>
+   <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/bootstrap-theme.css">
+</head>
+<body>
+  
+    <div class="cont">
+ <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+ $connect = mysqli_connect("localhost","root","","direpro");
+//$selectDb= mysqli_select_db($connect,"bd_dire_2");
+$email=$_REQUEST["email"];
+$query=mysqli_query($connect,"select * from users where user_email='$email'");
+$row=mysqli_fetch_array($query);
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+$mail = new PHPMailer();
+  try
+  {
+
+  $mail->SMTPDebug = 0;
+  $mail->isSMTP();
+  $mail->Host = "smtp.gmail.com";
+  $mail->SMTPAuth = TRUE;
+  $mail->Username = "bot.direpro@gmail.com";
+  $mail->Password = "botdirepro1995";
+  $mail->SMTPSecure = "false";
+  $mail->Port = 587;
+  $mail->From = "bot.direpro@gmail.com";
+  $mail->FromName = "DIREPRO";
+   $mail->addAddress($row["user_email"]);
+  $mail->isHTML(true);
+  $mail->Subject = "Password recuperada";
+  $mail->Body = "<i>Tu contraseña es :</i>".$row["user_password_hash"];
+  $mail->AltBody = "Gracias por confiar en nustra Seguridad de Datos";
+  $mail->send();
+    print("<p>Contraseña enviada a su correo registrado</p>");
+  }catch(Exception $e) {
+    echo 'Problemas al enviar', $mail->ErrorInfo;
+  }
+  ?> 
+  
+      <br>
+        <br>
+        <br>
+        <br>
+
+
+  <a href="login.php"><i class="glyphicon glyphicon-arrow-left"></i> Regresar al Login</a>
+      </div>
+
+<scrip src="js/jquery.min.js"></scrip>
+<scrip src="js/bootstrap.min.js"></scrip>
+</body>
+</html>
---
+++ DIREPRO_NEW/modal/registro_clientes.php
@@ -0,0 +1,70 @@
+	<?php
+		if (isset($con))
+		{
+	?>
+	<!-- Modal -->
+	<div class="modal fade" id="nuevoCliente" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	  <div class="modal-dialog" role="document">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Agregar Empresa</h4>
+		  </div>
+		  <div class="modal-body">
+			<form class="form-horizontal" method="post" id="guardar_cliente" name="guardar_cliente">
+			<div id="resultados_ajax"></div>
+			  <div class="form-group">
+				<label for="nombre" class="col-sm-3 control-label">Nombre</label>
+				<div class="col-sm-8">
+				  <input type="text" class="form-control" placeholder="Nombre" id="nombre" name="nombre" required>
+				</div>
+			  </div>
+			  <div class="form-group">
+				<label for="telefono" class="col-sm-3 control-label">RUC</label>
+				<div class="col-sm-8">
+				  <input type="text" class="form-control" placeholder="RUC"  id="telefono" name="telefono" >
+				</div>
+			  </div>
+
+			  <div class="form-group">
+				<label for="email" class="col-sm-3 control-label">Email</label>
+				<div class="col-sm-8">
+					<input type="email" class="form-control" placeholder="Email"  id="email" name="email" >
+
+				</div>
+			  </div>
+
+			  <div class="form-group">
+				<label for="direccion" class="col-sm-3 control-label">Teléfono</label>
+				<div class="col-sm-8">
+					<input type="direccion" class="form-control" placeholder="Teléfono"  id="direc" name="direccion">
+
+				</div>
+			  </div>
+
+			  <div class="form-group">
+				<label for="estado" class="col-sm-3 control-label">Estado</label>
+				<div class="col-sm-8">
+				 <select class="form-control" id="estado" name="estado" required>
+					<option value="">-- Selecciona estado --</option>
+					<option value="1" selected>Activo</option>
+					<option value="0">Inactivo</option>
+				  </select>
+				</div>
+			  </div>
+
+
+
+
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+			<button type="submit" class="btn btn-primary" id="guardar_datos">Guardar datos</button>
+		  </div>
+		  </form>
+		</div>
+	  </div>
+	</div>
+	<?php
+		}
+	?>
